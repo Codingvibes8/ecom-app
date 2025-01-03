b529bc0dd64477527ba0d616593910fd2d6a268f
Codingvibes8 <wecodeappz@gmail.com>
2025-01-03 08:53:53 +0000

    add nav

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Dayozona'
+export const APP_SLOGAN =
+    process.env.NEXT_PUBLIC_APP_SLOGAN || 'BEST QUALITY, SPEND LESS.'
+export const APP_DESCRIPTION =
+    process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+    'An ecommerce online app built with Next.js and MongoDB'

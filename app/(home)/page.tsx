a88fb7c48b90d30c087fdfe774c807539c870d8d
Codingvibes8 <wecodeappz@gmail.com>
2025-01-03 09:11:14 +0000

    modified

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,10 @@
 
-
+import {Button} from '@/components/ui/button'
 export default async function Page() {
   return (
       <div>
         <h1 className='h1-bold text-center p-10'>Home Page Content</h1>
+          <Button>Button</Button>
       </div>
   )
 }

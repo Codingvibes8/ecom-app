bdb771d7b5f019a6417cd067f2e5ea610972abcf
Codingvibes8 <wecodeappz@gmail.com>
2025-01-04 00:58:58 +0000

    modified

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,7 @@
 
-import {Button} from '@/components/ui/button'
+import { HomeCarousel } from '@/components/shared/home/home-carousel'
+import data from '@/lib/data'
+
 export default async function Page() {
-  return (
-      <div>
-        <h1 className='h1-bold text-center p-10'>Home Page Content</h1>
-          <Button>Button</Button>
-      </div>
-  )
+    return <HomeCarousel items={data.carousels} />
 }
--- components/shared/header/Header.tsx
+++ components/shared/header/Header.tsx
@@ -1,31 +1,36 @@
-import { APP_NAME } from '@/lib/constants'
 import Image from 'next/image'
 import Link from 'next/link'
+//import { getAllCategories } from '@/lib/actions/product.actions'
 import Menu from './menu'
-import { Button } from '../../components/ui/button'
-import { MenuIcon } from 'lucide-react'
-import data from '@/lib/data'
 import Search from './search'
+import data from '@/lib/data'
+//import Sidebar from './sidebar'
+//import { getSetting } from '@/lib/actions/setting.actions'
+//import { getTranslations } from 'next-intl/server'
 
-export default function Header() {
+export default async function Header() {
+    //const categories = await getAllCategories()
+    //const { site } = await getSetting()
+    //const t = await getTranslations()
     return (
-        <header className='bg-amber-600  text-white'>
+        <header className='bg-black  text-white'>
             <div className='px-2'>
                 <div className='flex items-center justify-between'>
                     <div className='flex items-center'>
                         <Link
                             href='/'
-                            className='bg-yellow-300 flex items-center ring-4 rounded-3xl p-2 ring-amber-200 gap-2 header-button font-extrabold text-2xl m-1 cursor-pointer'
+                            className='flex items-center header-button font-extrabold text-2xl m-1 '
                         >
                             <Image
-                                src='/logo-maker.svg'
+                                src={site.logo}
                                 width={40}
                                 height={40}
-                                alt={`${APP_NAME} logo`}
+                                alt={`${app.name} logo`}
                             />
-                            <span className={'text-blue-900 shadow-amber-50 shadow-2xl'}>{APP_NAME}</span>
+                            {app.name}
                         </Link>
                     </div>
+
                     <div className='hidden md:block flex-1 max-w-xl'>
                         <Search />
                     </div>
@@ -36,21 +41,15 @@ export default function Header() {
                 </div>
             </div>
             <div className='flex items-center px-3 mb-[1px]  bg-gray-800'>
-                <Button
-                    variant='ghost'
-                    className='header-button flex items-center gap-1 text-base [&_svg]:size-6'
-                >
-                    <MenuIcon />
-                    All
-                </Button>
+                <Sidebar categories={categories} />
                 <div className='flex items-center flex-wrap gap-3 overflow-hidden   max-h-[42px]'>
                     {data.headerMenus.map((menu) => (
                         <Link
                             href={menu.href}
                             key={menu.href}
-                            className='header-button !p-2'
+                            className='header-button !p-2 '
                         >
-                            {menu.name}
+                            {t('Header.' + menu.name)}
                         </Link>
                     ))}
                 </div>
